ad8c361f70b06d10eea1233110c58a1eb073524a
arunkjojo <arunkjojo@gmail.com>
2023-02-20 16:05:58 +0530

    Header image fix

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,20 @@
 import React, { FC } from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar,Image } from 'react-bootstrap';
 import {APP_DETAILS} from '../utils/constants'
+import logo from '../assets/svg/logo.svg'
 const Header:FC = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container fluid className='header-container'>
                 <Navbar.Brand className="brand" href="#home">
-                    <img
+                    {/* <img
                         alt={APP_DETAILS.APP_NAME}
                         src={`../../assets/svg/logo.svg`}
                         width="30"
                         height="30"
                         className="brand-img d-inline-block align-top"
-                    />{' '}
+                    /> */}
+                    <Image width="30" height="30" className="brand-img d-inline-block align-top" src={`${logo}`} alt={`${logo}`} />
                     <span className="brand-text">{APP_DETAILS.APP_NAME}</span>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
--- frontend/src/utils/constants/index.ts
+++ frontend/src/utils/constants/index.ts
@@ -3,7 +3,7 @@ import { APP_DATA } from "../interfaces";
 
 export const APP_DETAILS:APP_DATA = {
     APP_NAME:'Illinodi.com',
-    APP_URL:'https://illinodi.com/',
+    APP_URL:'https://main--funny-dusk-d87052.netlify.app/',
     APP_BASE: '/',
 
 }
